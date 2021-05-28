ddc95a6998b1274441a6b6bd3edc840918352ad3
marilee79 <bel18037@byui.edu>
2021-05-27 23:34:45 -0600

    feedback changes

---
+++ lesson6/js/index.js
@@ -0,0 +1,18 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+let d = new Date();
+document.querySelector('#theyear').textContent= d.getFullYear();
+
+const datefield = document.querySelector("date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.textContent = fulldate;
+
+if (now.getDay() == 5) { document.querySelector('aside').style.display = 'block';}
