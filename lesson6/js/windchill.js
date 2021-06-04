b9cbc8962bd2c4b813286714167b912fac578624
marilee79 <bel18037@byui.edu>
2021-06-03 23:48:09 -0600

    lazy load

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,17 +1,20 @@
-let temp = document.querySelector('#temp');
-let speed = document.querySelector('#wind');
-let wc = " ";
+let temp = document.querySelector('#temp').textContent;
+let speed = document.querySelector('#wind').textContent;
 
-function windChill(temp, speed) {
-    wc = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * (Math.pow(speed, 0.16)));
-    return wc; 
-}
+var wc = 0;
+var message = 0;
+
+//function windChill(temp, speed) {
+    //wc = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * (Math.pow(speed, 0.16)));
+    //return wc; 
+//}
 
 if (temp <= 50 && speed >= 3) {
-    wc = windChill(temp, speed) + "&deg;F";
+    wc = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * (Math.pow(speed, 0.16)));
+    message = wc.toFixed() + "&deg; F"; //wc = windChill(temp, speed) + "&deg;F";
     }
     else {
-        wc = "n/a";
+        message = "N/A";
     }
 
-document.querySelector('#windchill').innerHTML = wc;
+document.querySelector('#windchill').innerHTML = message;
