b6bd8b3747238d31909a78fd597e6c5b4253618b
marilee79 <bel18037@byui.edu>
2021-06-21 18:42:22 -0600

    medium view

--- lesson9/js/towndata.js
+++ lesson9/js/towndata.js
@@ -5,41 +5,39 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject); }) // temporary checking for valid response and data parsing
     const towns = jsonObject['towns'];
 
     const trifecta = towns.filter(town => town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven');
     // for (let i = 0; i < prophets.length; i++ ) { 
     trifecta.forEach(town => {
       let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let motto = document.createElement('div');
-      let founded = document.createElement('div');
-      let population = document.createElement('div');
-      let rainfall = document.createElement('div');
+      let h3 = document.createElement('h3');
+      let motto = document.createElement('h4');
+      let founded = document.createElement('p');
+      let population = document.createElement('p');
+      let rainfall = document.createElement('p');
+      let data = document.createElement('div');
       let image = document.createElement('img');
 
-      h2.textContent = `${town.name}`;
+      h3.textContent = `${town.name}`;
       motto.textContent = `${town.motto}`;
-      founded.textContent = 'Year Founded:' + ' ' + town.yearFounded;
-      population.textContent = 'Current Population:' + ' ' + town.currentPopulation;
-      rainfall.textContent = 'Average Rain Fall:' + ' ' + town.averageRainfall;
+      founded.textContent = 'Founded: ' + town.yearFounded;
+      population.textContent = 'Current Population: ' + town.currentPopulation;
+      rainfall.textContent = 'Average Rainfall: ' + town.averageRainfall;
+      
       image.setAttribute('src', `images/${town.photo}`);
       image.setAttribute('alt', `${town.name}`);
-      h2.setAttribute('class', 'data');
-      motto.setAttribute('class', 'mottodiv');
-      founded.setAttribute('class', 'foundeddiv');
-      population.setAttribute('class', 'populationdiv');
-      rainfall.setAttribute('class', 'fainfalldiv');
-
+      data.setAttribute('class', 'data');
       
-      card.appendChild(h2);
-      card.appendChild(motto);
-      card.appendChild(founded);
-      card.appendChild(population);
-      card.appendChild(rainfall);
-      card.appendChild(image);
+      
+      card.append(h3);
+      card.append(motto);
+      card.append(data);
+      data.append(founded);
+      data.append(population);
+      data.append(rainfall);
+      card.append(image);
 
-      document.querySelector('div.towns').appendChild(card);
+      document.querySelector('div.towns').append(card);
     }) 
   });   
