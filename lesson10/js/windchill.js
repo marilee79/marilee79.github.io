c826084d942d7f6bf9e68cc72020bd31f44c1bf3
marilee79 <bel18037@byui.edu>
2021-06-26 22:26:37 -0600

    five day

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,5 +1,20 @@
-let temp = document.querySelector('#temp').textContent;
-let speed = document.querySelector('#wind').textContent;
+const cityid = "5604473";
+const APPID = "fe5ce56ddb1739e03b5b9bb24fc93e2e";
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        const current = document.querySelector('#current');
+        const temp = document.querySelector('#temp');
+        const speed = document.querySelector('#wind');
+        const humidity = document.querySelector('#humidity');
+        current.textContent = jsObject.main.temp.toFixed(0);
+        temp.textContent = jsObject.main.temp_max.toFixed(0);
+        speed.textContent = jsObject.wind.speed.toFixed(0);
+        humidity.textContent = jsObject.main.humidity;
 
 var wc = 0;
 var message = 0;
@@ -13,3 +28,5 @@ if (temp <= 50 && speed >= 3) {
     }
 
 document.querySelector('#windchill').innerHTML = message;
+    
+    });
