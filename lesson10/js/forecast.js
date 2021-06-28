d69b43bbb31010419495c12dc6c6586c0c98a403
marilee79 <bel18037@byui.edu>
2021-06-27 23:53:27 -0600

    forecast changes

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,19 +1,19 @@
 const cityId = "5604473"
 const appid = "fe5ce56ddb1739e03b5b9bb24fc93e2e";
-const apitURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${appid}&units=imperial`;
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${appid}&units=imperial`;
 
-fetch(apiURL)
+fetch(forecastURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        // console.log(jsObject);
         let day = 0;
         const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        const fiveDayForecast = jsObject.list.filter( forecast => forecast.dt_txt('18:00:00'));
-        // console.log(fiveDayForecast);
+        const fiveDayForecast = jsObject.list.filter((forecast)=>forecast.dt_txt.includes('18:00:00'));
+        console.log(fiveDayForecast);
 
-        fiveDayForecast.forEach( x => {
+        fiveDayForecast.forEach(x => {
             let d = new Date(x.dt_txt);
-            //console.log(d);
+            // console.log(d);
             document.getElementById(`weekday${day+1}`).textContent = weekday[d.getDay()];
             document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
             day++
