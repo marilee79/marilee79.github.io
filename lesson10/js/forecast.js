3eddb8ebc28ac7f35e4cf5295e84a09d5dde056c
marilee79 <bel18037@byui.edu>
2021-07-01 01:44:54 -0600

    make changes

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -7,22 +7,22 @@ fetch(forecastURL)
     .then((jsObject) => {
         // console.log(jsObject);
         let day = 0;
-        const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
         const fiveDayForecast = jsObject.list.filter((forecast)=>forecast.dt_txt.includes('18:00:00'));
         console.log(fiveDayForecast);
 
         fiveDayForecast.forEach(x => {
             let d = new Date(x.dt_txt);
-            // console.log(d);
+            console.log(d);
             document.getElementById(`weekday${day+1}`).textContent = weekday[d.getDay()];
             document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
-            day++
+            day++        
         });
-
-        const desc = jsObject.weather[0].description;
-        const image = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        // const desc = jsObject.weather[0].icon;
+        // const image = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
         
-        document.getElementById('icon').setAttribute('src', image);
-        document.getElementById('icon').setAttribute('alt', desc);
+        // document.getElementById(`icon`).setAttribute('src', image);
+        // document.getElementById(`icon`).setAttribute('alt', desc);
 
+        
     });
